57d41727f4c4f7c1bd2f26836c9e7fc66d28f9cf
ZhengjieZhang <124875925+zhangzj1209@users.noreply.github.com>
2023-05-06 22:48:08 +0800

    Add files via upload

---
+++ xcorr/config_xcorr.py
@@ -0,0 +1,10 @@
+class Config(object):
+    def __init__(self):
+        # Path
+        self.root = 'D:/PyCharm/Code/xcorr'
+
+        # Other parameters
+        self.original_signal_length = 1500   # data length 6s * 250Hz
+        self.sample = 250       # Hz
+        self.window = 6         # sec
+        self.step = 4           # sec
